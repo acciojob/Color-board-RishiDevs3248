6eac38697cbf1bee8b67eceb7e279fa2973990fc
Hrishikesh Alabnur <144805491+RishiDevs3248@users.noreply.github.com>
2025-09-25 10:32:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ for (let i = 0; i < 800; i++) {
 		square.style.transition = "none";
 	})
 	square.addEventListener('mouseout',()=>{
-		square.style.backgroundColor = "gray";
+		square.style.backgroundColor = "rgb(29, 29, 29)";
 		square.style.transition = "background-color 1s ease";
 	})
 	container.appendChild(square);
